344a86080aeb4e927b05a23408103dba58be6d27
yuvarajmms <31970667+yuvarajmms@users.noreply.github.com>
2020-04-03 11:13:46 +0530

    logger added

--- src/main/java/com/test/app/service/SaleService.java
+++ src/main/java/com/test/app/service/SaleService.java
@@ -6,18 +6,22 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.test.app.entity.Sales;
 import com.test.app.repository.SaleRepository;
 
 @Service
 public class SaleService {
-
+	
+	Logger logger = LoggerFactory.getLogger(SaleService.class);
+	
 	@Autowired
 	SaleRepository saleRepo;
 	
 	public List<Sales> getAllSales(){
 		List<Sales> saleList =saleRepo.findAll();
+		logger.info("Sales List size:"+saleList.size());
 		if(saleList.size() > 0){
 			return saleList;
 		} else {
@@ -31,6 +35,7 @@ public class SaleService {
 	}
 	
 	public void deleteById(Integer id){
+		logger.info("Delete sale for:"+id);
 		saleRepo.deleteById(id);
 	}
 	
